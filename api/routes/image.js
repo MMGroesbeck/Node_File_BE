9dc51e4cdbc2c948dc8919863df13867e6399f12
M Groesbeck <m.groesbeck@gmail.com>
2021-01-03 09:10:49 -0800

    Build image eCRD endpoints.

--- api/routes/image.js
+++ api/routes/image.js
@@ -1,6 +1,93 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const fs = require("fs");
 
-const Image = require('../models/imageModel');
-const User = require('../models/userModel');
+const authenticator = require("./middleware/authenticator");
+
+const File = require("../models/fileModel");
+const Image = require("../models/imageModel");
+
+router.post(
+  "/",
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: true }),
+  (req, res) => {
+    try {
+      if (!req.files) {
+        res.status(400).json({ message: "No file uploaded." });
+      } else {
+        let image = req.files.image;
+        let image_data = {
+          user_id: req.decodedToken.userId,
+          public: req.body.public,
+          metadata: req.body.metadata,
+        };
+        Image.add(image_data)
+          .then((new_image) => {
+            const new_image_id = new_image.file_uuid;
+            File.add(image, new_image_id)
+              .then((resp) => {
+                if (!resp) {
+                  res.status(201).json({ message: "Image uploaded." });
+                } else {
+                  res.status(500).json({ error: resp });
+                }
+              })
+              .catch((err) => {
+                res.status(500).json({ errorMessage: err.message });
+              });
+          })
+          .catch((err) => {
+            res.status(500).json({ errorMessage: err.message });
+          });
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+);
+
+router.get("/:id", (req, res) => {
+    const id = req.params.id;
+    Image.findById(id)
+        .then(img => {
+            if (img.public || img.user_id === req.decodedToken.userId) {
+                res.download("/images" + id, id, err => {
+                    if (err) {
+                        res.status(500).json({ error: err });
+                    }
+                });
+            } else {
+                res.status(403).json({ message: "User does not have access to this image." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: err.message });
+        })
+})
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    Image.findById(id)
+        .then(img => {
+            if (img.user_id === req.decodedToken.userId) {
+                File.unlink(img.file_uuid)
+                    .then(resp => {
+                        if (!resp) {
+                            Image.remove(id)
+                                .then(removed => {
+                                    res.status(200).json({ message: "Image deleted." });
+                                })
+                                .catch(err => {
+                                    res.status(500).json({ message: "Error removing image from db." });
+                                })
+                        } else {
+                            res.status(500).json({ message: "Error unlinking image." });
+                        }
+                    })
+            } else {
+                res.status(403).json({ message: "Cannot delete image uploaded by other user." });
+            }
+        })
+})
 
 module.exports = router;
